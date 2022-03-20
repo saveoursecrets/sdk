c2e7d8fe75561c35a409ef635eb4bb6024eba78a
muji <muji@tmpfs.org>
2022-03-20 17:57:46 +0800

    Abstraction for server backend.
    Will allow us in the future to implement different backing storage such
    as s3.

--- workspace/server/src/bin/main.rs
+++ workspace/server/src/bin/main.rs
@@ -1,16 +1,12 @@
-use anyhow::{bail, Result};
+use anyhow::Result;
 use clap::Parser;
-use sos3_server::{Server, State};
-use sos_core::vault::Vault;
+use sos3_server::{FileSystemBackend, Server, State};
 use std::{
-    collections::HashMap,
-    fs::read_dir,
     net::SocketAddr,
     path::PathBuf,
     str::FromStr,
     sync::{Arc, RwLock},
 };
-use uuid::Uuid;
 
 /// Secret storage server.
 #[derive(Parser, Debug)]
@@ -28,37 +24,15 @@ struct Cli {
 #[tokio::main]
 async fn main() -> Result<()> {
     let args = Cli::parse();
-    let vaults = read_vaults(&args.dir)?;
-    let state = Arc::new(RwLock::new(State { vaults }));
+
+    let mut backend = FileSystemBackend::new(args.dir.clone());
+    backend.read_dir()?;
+
+    let state = Arc::new(RwLock::new(State {
+        backend: Box::new(backend),
+    }));
+
     let addr = SocketAddr::from_str(&args.bind)?;
     Server::start(addr, state).await;
     Ok(())
-}
-
-/// Read vaults into memory.
-fn read_vaults(dir: &PathBuf) -> Result<HashMap<Uuid, Vault>> {
-    if !dir.is_dir() {
-        bail!("not a directory {}", dir.display());
-    }
-
-    let mut vaults = Vec::new();
-    for entry in read_dir(dir)? {
-        let entry = entry?;
-        let path = entry.path();
-        if let Some(ext) = path.extension() {
-            if ext == Vault::extension() {
-                let vault = Vault::read_file(path)?;
-                vaults.push(vault);
-            }
-        }
-    }
-
-    if vaults.is_empty() {
-        bail!("no vaults found");
-    }
-
-    Ok(vaults
-        .into_iter()
-        .map(|v| (v.id().clone(), v))
-        .collect::<_>())
 }
