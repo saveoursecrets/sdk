bdf34014cf98a34e3a4eba917363f486c1a1d112
muji <muji@tmpfs.org>
2022-03-23 10:14:27 +0800

    Sketch secret list and UI elements.

--- sandbox/src/vault.tsx
+++ sandbox/src/vault.tsx
@@ -1,14 +1,16 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from "react-redux";
 import { WebVault } from 'sos-wasm';
 import { useNavigate, useParams } from 'react-router-dom';
 
 import Typography from '@mui/material/Typography';
 import Link from '@mui/material/Link';
+import Divider from '@mui/material/Divider';
 
 import { VaultWorker } from './worker';
 import { WorkerContext } from "./worker-provider";
 import { vaultsSelector, VaultStorage } from './store/vaults';
+import SecretList from "./secret-list";
 
 function downloadVault(fileName: string, buffer: Uint8Array) {
     const blob = new Blob([buffer], {type: "application/octet-stream"});
@@ -26,6 +28,7 @@ interface VaultViewProps {
 function VaultView(props: VaultViewProps) {
   const { worker, storage } = props;
   const { vault, uuid, label } = storage;
+  const [secrets, setSecrets] = useState(null);
 
   const download = async (
       e: React.MouseEvent<HTMLElement>
@@ -35,6 +38,16 @@ function VaultView(props: VaultViewProps) {
     downloadVault(`${uuid}.vault`, buffer);
   }
 
+  useEffect(() => {
+    const getSecretsMeta = async () => {
+      const secrets = await vault.getSecretIndex();
+      const secretsMap = new Map(Object.entries(secrets));
+      console.log("loaded secrets", secretsMap);
+      setSecrets(() => secretsMap);
+    }
+    getSecretsMeta();
+  }, [storage]);
+
   return <>
     <Typography variant="h3" gutterBottom component="div">
         {label}
@@ -43,6 +56,8 @@ function VaultView(props: VaultViewProps) {
         {uuid}
     </Typography>
     <Link href="#" onClick={(e) => download(e)}>Download</Link>
+    <Divider />
+    <SecretList worker={worker} secrets={secrets} />
   </>;
 }
 
