b56e847e53a628e964d11f0e30d4694472db34ff
muji <muji@tmpfs.org>
2022-06-18 14:23:11 +0800

    Remove outer runtime.
    The outer tokio runtime was not necessary and removing it means we could
    remove the 'static lifetime requirement and create a new runtime for
    each client invocation from the shell prompt. Much simpler.

--- workspace/client/src/bin/main.rs
+++ workspace/client/src/bin/main.rs
@@ -1,11 +1,10 @@
-use std::{fs::File, io::Read, path::PathBuf, sync::Arc, thread::spawn};
+use std::{fs::File, io::Read, path::PathBuf, sync::Arc};
 
 use clap::{CommandFactory, Parser, Subcommand};
 use std::future::Future;
 use tokio::runtime::Runtime;
 use url::Url;
 use web3_keystore::{decrypt, KeyStore};
-use once_cell::sync::OnceCell;
 
 use sos_client::{Client, Result};
 use sos_core::signer::SingleParty;
@@ -38,36 +37,19 @@ enum ShellCommand {
     ListVaults {},
 }
 
-/// Hack to give the client a 'static lifetime which is required
-/// to pass the futures created into the spawned thread used to block
-/// the shell prompt.
-fn global_client(value: Option<(Url, SingleParty)>) -> &'static Arc<Client> {
-    static INSTANCE: OnceCell<Arc<Client>> = OnceCell::new();
-    INSTANCE.get_or_init(|| {
-        let value = value.unwrap();
-        Arc::new(Client::new(value.0, Arc::new(value.1)))
-    })
-}
-
-/// Runs a future in a separate thread and runtime so we can
+/// Runs a future blocking the current thread so we can
 /// merge the synchronous nature of the shell prompt with the
 /// asynchronous API exposed by the client.
 fn run_blocking<F, R>(func: F) -> Result<R>
 where
-    F: Future<Output = Result<R>> + Send + Sync + 'static,
-    R: Send + Sync + 'static
+    F: Future<Output = Result<R>> + Send + Sync,
+    R: Send + Sync
 {
-    let handle = spawn(move || {
-        Runtime::new().unwrap().block_on(func)
-    });
-
-    let result = handle.join().unwrap();
-    let inner = result?;
-    return Ok(inner);
+    Ok(Runtime::new().unwrap().block_on(func)?)
 }
 
-fn run_shell_command(line: &str) -> Result<()> {
-    let client = global_client(None);
+fn run_shell_command(line: &str, client: Arc<Client>) -> Result<()> {
+    //let client = global_client(None);
     let prefixed = format!("sos-shell {}", line);
     let it = prefixed.split_ascii_whitespace();
     let mut cmd = Shell::command();
@@ -110,7 +92,7 @@ fn welcome(api: &Url) -> Result<()> {
     Ok(())
 }
 
-async fn run() -> Result<()> {
+fn run() -> Result<()> {
     let args = Cli::parse();
 
     // Decrypt the keystore and create the client.
@@ -124,13 +106,14 @@ async fn run() -> Result<()> {
 
     let signing_key: [u8; 32] = signing_bytes.as_slice().try_into()?;
     let signer: SingleParty = (&signing_key).try_into()?;
-    let client = global_client(Some((args.api, signer)));
+    let client = Arc::new(Client::new(args.api, Arc::new(signer)));
 
     welcome(client.api())?;
 
     read_shell(
         |line: String| {
-            run_shell_command(&line).unwrap();
+            let client = Arc::clone(&client);
+            run_shell_command(&line, client).unwrap();
         },
         Some("sos> "),
     )?;
@@ -138,9 +121,8 @@ async fn run() -> Result<()> {
     Ok(())
 }
 
-#[tokio::main]
-async fn main() -> Result<()> {
-    match run().await {
+fn main() -> Result<()> {
+    match run() {
         Ok(_) => {}
         Err(e) => {
             eprintln!("{}", e);
