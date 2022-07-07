4f0814894742c0902afbfb2dcd84ce413d3e6b7e
muji <muji@tmpfs.org>
2022-07-07 17:47:35 +0800

    Trying new sources list.

--- mk/install-build-tools.sh
+++ mk/install-build-tools.sh
@@ -41,7 +41,19 @@ case $target in
   # Clang is needed for code coverage.
   #use_clang=1
 
-  echo 'deb [arch=arm64] http://bg.ports.ubuntu.com/ focal main restricted' | sudo tee -a /etc/apt/sources.list
+  SOURCES="deb [arch=amd64] http://azure.archive.ubuntu.com/ubuntu/ focal main restricted
+deb [arch=amd64] http://azure.archive.ubuntu.com/ubuntu/ focal-updates main restricted
+deb [arch=amd64] http://azure.archive.ubuntu.com/ubuntu/ focal universe
+deb [arch=amd64] http://azure.archive.ubuntu.com/ubuntu/ focal-updates universe
+deb [arch=amd64] http://azure.archive.ubuntu.com/ubuntu/ focal multiverse
+deb [arch=amd64] http://azure.archive.ubuntu.com/ubuntu/ focal-updates multiverse
+deb [arch=amd64] http://azure.archive.ubuntu.com/ubuntu/ focal-backports main restricted universe multiverse
+deb [arch=amd64] http://azure.archive.ubuntu.com/ubuntu/ focal-security main restricted
+deb [arch=amd64] http://azure.archive.ubuntu.com/ubuntu/ focal-security universe
+deb [arch=amd64] http://azure.archive.ubuntu.com/ubuntu/ focal-security multiverse
+deb [arch=arm64] http://bg.ports.ubuntu.com/ focal main restricted"
+
+  echo "$SOURCES" | sudo tee /etc/apt/sources.list
 
   sudo cat /etc/apt/sources.list
 
