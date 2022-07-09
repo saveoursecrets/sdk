c59211ba16049f0dd71b1c31ea4c076fbe4769c4
muji <muji@tmpfs.org>
2022-07-09 17:59:57 +0800

    Audit monitor (#49)
    * Experiment with audit monitor.
    
    * Add monitor command to the audit tool.

--- workspace/audit/src/bin/main.rs
+++ workspace/audit/src/bin/main.rs
@@ -5,7 +5,7 @@ use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 use sos_audit::Result;
 use sos_core::address::AddressStr;
 
-/// Print and monitor audit logs.
+/// Print and monitor audit log events.
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
 struct Cli {
@@ -15,14 +15,35 @@ struct Cli {
 
 #[derive(Subcommand, Debug)]
 enum Command {
-    /// Print the log records in an audit log file
-    #[clap(alias = "log")]
+    /// Print the events in an audit log file
     Logs {
-        /// Print each log record as a line of JSON
+        /// Print each event as a line of JSON
         #[clap(short, long)]
         json: bool,
 
-        /// Filter to records that match the given address.
+        /// Iterate from the end of the file
+        #[clap(short, long)]
+        reverse: bool,
+
+        /// Limit events to displayed to this count
+        #[clap(short, long)]
+        count: Option<usize>,
+
+        /// Filter to events that match the given address.
+        #[clap(short, long)]
+        address: Vec<AddressStr>,
+
+        /// Audit log file
+        #[clap(parse(from_os_str))]
+        audit_log: PathBuf,
+    },
+    /// Monitor changes to an audit log file
+    Monitor {
+        /// Print each event as a line of JSON
+        #[clap(short, long)]
+        json: bool,
+
+        /// Filter to events that match the given address.
         #[clap(short, long)]
         address: Vec<AddressStr>,
 
@@ -39,8 +60,17 @@ fn run() -> Result<()> {
             audit_log,
             json,
             address,
+            reverse,
+            count,
         } => {
-            sos_audit::logs(audit_log, json, address)?;
+            sos_audit::logs(audit_log, json, address, reverse, count)?;
+        }
+        Command::Monitor {
+            audit_log,
+            json,
+            address,
+        } => {
+            sos_audit::monitor(audit_log, json, address)?;
         }
     }
     Ok(())
--- workspace/audit/src/lib.rs
+++ workspace/audit/src/lib.rs
@@ -1,14 +1,6 @@
-use std::path::PathBuf;
+use std::{fs::File, path::PathBuf, thread, time};
 
-use sos_core::{
-    address::AddressStr,
-    constants::AUDIT_IDENTITY,
-    serde_binary::{
-        binary_rw::{BinaryReader, Endian, FileStream, OpenType, SeekStream},
-        Deserializer,
-    },
-    AuditData, AuditLogFile,
-};
+use sos_core::{address::AddressStr, AuditData, AuditEvent, AuditLogFile};
 
 mod error;
 
@@ -16,7 +8,8 @@ pub type Result<T> = std::result::Result<T, error::Error>;
 
 pub use error::Error;
 
-pub fn logs(
+/// Monitor changes in an audit log file.
+pub fn monitor(
     audit_log: PathBuf,
     json: bool,
     address: Vec<AddressStr>,
@@ -25,61 +18,123 @@ pub fn logs(
         return Err(Error::NotFile(audit_log));
     }
 
+    // File for iterating
     let log_file = AuditLogFile::new(&audit_log)?;
 
-    let mut stream = FileStream::new(&audit_log, OpenType::Open)?;
-    let mut reader = BinaryReader::new(&mut stream, Endian::Big);
-    reader.seek(AUDIT_IDENTITY.len())?;
+    // File for reading event data
+    let mut file = File::open(&audit_log)?;
 
-    let mut deserializer = Deserializer { reader };
+    let mut it = log_file.iter()?;
+    let mut offset = audit_log.metadata()?.len();
+    // Push iteration constraint to the end of the file
+    it.set_offset(offset as usize);
 
-    for _record in log_file.iter()? {
-        //println!("record: {:#?}", record);
-        let event = AuditLogFile::decode_row(&mut deserializer)?;
+    loop {
+        let step = time::Duration::from_millis(100);
+        thread::sleep(step);
 
-        if !address.is_empty() {
-            if address
-                .iter()
-                .position(|addr| addr == event.address())
-                .is_none()
-            {
+        let len = audit_log.metadata()?.len();
+        if len > offset {
+            while let Some(record) = it.next() {
+                let record = record?;
+                let event = log_file.read_event(&mut file, &record)?;
+                if !address.is_empty() && !is_address_match(&event, &address)
+                {
+                    continue;
+                }
+                print_event(event, json)?;
+            }
+
+            offset = len;
+
+            // Adjust the iterator constraint for the consumer records
+            it.set_offset(len as usize);
+        }
+    }
+}
+
+/// Print events in an audit log file.
+pub fn logs(
+    audit_log: PathBuf,
+    json: bool,
+    address: Vec<AddressStr>,
+    reverse: bool,
+    count: Option<usize>,
+) -> Result<()> {
+    if !audit_log.is_file() {
+        return Err(Error::NotFile(audit_log));
+    }
+
+    // File for iterating
+    let log_file = AuditLogFile::new(&audit_log)?;
+
+    // File for reading event data
+    let mut file = File::open(&audit_log)?;
+
+    let count = count.unwrap_or_else(|| usize::MAX);
+
+    if reverse {
+        for record in log_file.iter()?.rev().take(count) {
+            let record = record?;
+            let event = log_file.read_event(&mut file, &record)?;
+            if !address.is_empty() && !is_address_match(&event, &address) {
                 continue;
             }
+            print_event(event, json)?;
         }
-
-        if json {
-            println!("{}", serde_json::to_string(&event)?);
-        } else if let Some(data) = event.data() {
-            match data {
-                AuditData::Vault(vault_id) => {
-                    tracing::info!(
-                        vault = ?vault_id,
-                        "{} {} by {}",
-                        event.time().to_rfc3339()?,
-                        event.event_kind(),
-                        event.address(),
-                    );
-                }
-                AuditData::Secret(vault_id, secret_id) => {
-                    tracing::info!(
-                        vault = ?vault_id,
-                        secret = ?secret_id,
-                        "{} {} by {}",
-                        event.time().to_rfc3339()?,
-                        event.event_kind(),
-                        event.address(),
-                    );
-                }
+    } else {
+        for record in log_file.iter()?.take(count) {
+            let record = record?;
+            let event = log_file.read_event(&mut file, &record)?;
+            if !address.is_empty() && !is_address_match(&event, &address) {
+                continue;
             }
-        } else {
-            tracing::info!(
-                "{} {} by {}",
-                event.time().to_rfc3339()?,
-                event.event_kind(),
-                event.address(),
-            );
+            print_event(event, json)?;
         }
     }
 
     Ok(())
+}
+
+fn print_event(event: AuditEvent, json: bool) -> Result<()> {
+    if json {
+        println!("{}", serde_json::to_string(&event)?);
+    } else if let Some(data) = event.data() {
+        match data {
+            AuditData::Vault(vault_id) => {
+                tracing::info!(
+                    vault = ?vault_id,
+                    "{} {} by {}",
+                    event.time().to_rfc3339()?,
+                    event.event_kind(),
+                    event.address(),
+                );
+            }
+            AuditData::Secret(vault_id, secret_id) => {
+                tracing::info!(
+                    vault = ?vault_id,
+                    secret = ?secret_id,
+                    "{} {} by {}",
+                    event.time().to_rfc3339()?,
+                    event.event_kind(),
+                    event.address(),
+                );
+            }
+        }
+    } else {
+        tracing::info!(
+            "{} {} by {}",
+            event.time().to_rfc3339()?,
+            event.event_kind(),
+            event.address(),
+        );
+    }
+    Ok(())
+}
+
+fn is_address_match(event: &AuditEvent, address: &[AddressStr]) -> bool {
+    address
+        .iter()
+        .position(|addr| addr == event.address())
+        .is_some()
 }
