1310701ea1c3bc4ab08752ec2be1ea89ca002151
muji <muji@tmpfs.org>
2022-09-08 09:53:54 +0800

    Improve wrapper executable help.
    Based on the clap updates described in this issue:
    
    https://github.com/clap-rs/clap/issues/3887

--- src/error.rs
+++ src/error.rs
@@ -2,12 +2,13 @@ use thiserror::Error;
 
 #[derive(Debug, Error)]
 pub enum Error {
+    /*
     #[error("subcommand is required")]
     CommandRequired,
 
     #[error("{0} is not a known command")]
     UnknownCommand(String),
-
+    */
     #[error(transparent)]
     Io(#[from] std::io::Error),
 }
