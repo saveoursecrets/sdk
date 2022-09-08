ea334a1f41638dd3ba3035c2fe15d57aad924538
muji <muji@tmpfs.org>
2022-09-08 09:58:59 +0800

    Improve wrapper executable help. (#98)
    Based on the clap updates described in this issue:
    
    https://github.com/clap-rs/clap/issues/3887

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,96 @@
-use clap::Parser;
+use clap::{Parser, Subcommand};
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
-use sos::{Error, Result};
+use sos::Result;
 
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
-enum Sos {
-    #[clap(external_subcommand)]
-    Command(Vec<String>),
+struct Sos {
+    #[clap(subcommand)]
+    cmd: Command,
 }
 
-// NOTE: Currently help output is not what we would like.
-// SEE:  https://github.com/clap-rs/clap/issues/3887
+#[derive(Debug, Subcommand)]
+enum Command {
+    /// Key agent service.
+    #[clap(
+        allow_external_subcommands = true,
+        trailing_var_arg = true,
+        allow_hyphen_values = true,
+        disable_version_flag = true,
+        disable_help_flag = true,
+        disable_help_subcommand = true
+    )]
+    Agent {},
+    /// Print and monitor audit log events.
+    #[clap(
+        allow_external_subcommands = true,
+        trailing_var_arg = true,
+        allow_hyphen_values = true,
+        disable_version_flag = true,
+        disable_help_flag = true,
+        disable_help_subcommand = true
+    )]
+    Audit {},
+    /// Utility tool to check status and integrity.
+    #[clap(
+        allow_external_subcommands = true,
+        trailing_var_arg = true,
+        allow_hyphen_values = true,
+        disable_version_flag = true,
+        disable_help_flag = true,
+        disable_help_subcommand = true
+    )]
+    Check {},
+    /// Secret storage interactive shell.
+    #[clap(
+        allow_external_subcommands = true,
+        trailing_var_arg = true,
+        allow_hyphen_values = true,
+        disable_version_flag = true,
+        disable_help_flag = true,
+        disable_help_subcommand = true
+    )]
+    Client {},
+    /// Secret storage server.
+    #[clap(
+        allow_external_subcommands = true,
+        trailing_var_arg = true,
+        allow_hyphen_values = true,
+        disable_version_flag = true,
+        disable_help_flag = true,
+        disable_help_subcommand = true
+    )]
+    Server {},
+}
 
 fn run() -> Result<()> {
-    let args = Sos::parse();
-    match args {
-        Sos::Command(mut args) => {
-            if let Some(_) = args.get(0) {
-                let command = args.remove(0);
-                match &command[..] {
-                    "agent" | "audit" | "check" | "client" | "server" => {
-                        let cmd = format!("sos-{}", command);
-                        std::process::Command::new(&cmd)
-                            .args(args)
-                            .status()?;
-                        Ok(())
-                    }
-                    _ => Err(Error::UnknownCommand(command)),
-                }
-            } else {
-                Err(Error::CommandRequired)
-            }
-        }
-    }
+    let argv = Sos::parse();
+    let args = std::env::args().skip(2).collect::<Vec<_>>();
+    let cmd = match argv.cmd {
+        Command::Agent {} => "sos-agent",
+        Command::Audit {} => "sos-audit",
+        Command::Check {} => "sos-check",
+        Command::Client {} => "sos-client",
+        Command::Server {} => "sos-server",
+    };
+
+    let cmd = if cfg!(debug_assertions) {
+        format!("target/debug/{}", cmd)
+    } else {
+        cmd.to_owned()
+    };
+
+    tracing::debug!(cmd = %cmd, args = ?args);
+
+    std::process::Command::new(&cmd).args(args).status()?;
+    Ok(())
 }
 
 fn main() -> Result<()> {
     tracing_subscriber::registry()
         .with(tracing_subscriber::EnvFilter::new(
-            std::env::var("RUST_LOG").unwrap_or_else(|_| "sos=info".into()),
+            std::env::var("RUST_LOG").unwrap_or_else(|_| "info".into()),
         ))
         .with(tracing_subscriber::fmt::layer().without_time())
         .init();
