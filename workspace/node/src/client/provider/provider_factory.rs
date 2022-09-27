7036f5692cc0967112cee6adf073679928cdf234
muji <muji@tmpfs.org>
2022-09-28 07:35:43 +0800

    Experiment with provider enum proxy.
    Doesn't appear to work because of the trait generics.

--- workspace/node/src/client/provider/provider_factory.rs
+++ workspace/node/src/client/provider/provider_factory.rs
@@ -1,11 +1,15 @@
 //! Factory for creating providers.
+use async_trait::async_trait;
 use sos_core::{
+    events::{WalEvent},
     signer::BoxedSigner,
-    wal::{file::WalFile, WalProvider, memory::WalMemory},
+    wal::{file::WalFile, WalProvider, memory::WalMemory, snapshot::SnapShotManager},
     PatchFile, PatchProvider, PatchMemory,
+    vault::{Summary, Vault, VaultId},
 };
 use std::{
     fmt,
+    collections::HashMap,
     path::PathBuf,
     str::FromStr,
     sync::{Arc, RwLock},
@@ -20,7 +24,7 @@ use crate::{
         net::RpcClient,
         provider::{
             LocalProvider, RemoteProvider, StorageDirs, StorageProvider,
-            BoxedProvider,
+            BoxedProvider, ProviderState,
         },
         Error, Result,
     },
@@ -29,6 +33,7 @@ use crate::{
 /// Provider that can be safely sent between threads.
 pub type ArcProvider<W, P> = Arc<RwLock<BoxedProvider<W, P>>>;
 
+/*
 /// Enumeration of the provider concrete types.
 pub enum Provider {
     /// Storage provider backed by files on disc.
@@ -37,11 +42,109 @@ pub enum Provider {
     Memory(BoxedProvider<WalMemory, PatchMemory<'static>>),
 }
 
+macro_rules! enum_proxy_call {
+    ( $name:ident, $self:expr $( , $arg:ident )* ) => {
+        match $self {
+            Self::File(o) => o.$name($($arg),*),
+            Self::Memory(o) => o.$name($($arg),*),
+        }
+    };
+}
+
+macro_rules! enum_proxy_call_async {
+    ( $name:ident, $self:expr $( , $arg:ident )* ) => {
+        match $self {
+            Self::File(o) => o.$name($($arg),*).await,
+            Self::Memory(o) => o.$name($($arg),*).await,
+        }
+    };
+}
+
+#[cfg_attr(target_arch="wasm32", async_trait(?Send))]
+#[cfg_attr(not(target_arch = "wasm32"), async_trait)]
+impl StorageProvider<WalFile, PatchFile> for Provider {
+    fn state(&self) -> &ProviderState {
+        enum_proxy_call!(state, self)
+    }
+
+    fn state_mut(&mut self) -> &mut ProviderState {
+        enum_proxy_call!(state_mut, self)
+    }
+
+    fn dirs(&self) -> &StorageDirs {
+        enum_proxy_call!(dirs, self)
+    }
+
+    fn cache(&self) -> &HashMap<VaultId, (WalFile, PatchFile)> {
+        enum_proxy_call!(cache, self)
+    }
+
+    fn cache_mut(&mut self) -> &mut HashMap<VaultId, (WalFile, PatchFile)> {
+        enum_proxy_call!(cache_mut, self)
+    }
+
+    fn snapshots(&self) -> Option<&SnapShotManager> {
+        enum_proxy_call!(snapshots, self)
+    }
+
+    async fn update_vault<'a>(
+        &mut self,
+        summary: &Summary,
+        vault: &Vault,
+        events: Vec<WalEvent<'a>>,
+    ) -> Result<()> {
+        enum_proxy_call_async!(update_vault, self, summary, vault, events)
+    }
+
+    async fn compact(&mut self, summary: &Summary) -> Result<(u64, u64)>;
+    async fn refresh_vault(
+        &mut self,
+        summary: &Summary,
+        new_passphrase: Option<&SecretString>,
+    ) -> Result<()>;
+    async fn create_account_with_buffer(
+        &mut self,
+        buffer: Vec<u8>,
+    ) -> Result<Summary>;
+    async fn create_vault_or_account(
+        &mut self,
+        name: Option<String>,
+        passphrase: Option<String>,
+        _is_account: bool,
+    ) -> Result<(SecretString, Summary)>;
+    async fn remove_vault(&mut self, summary: &Summary) -> Result<()>;
+    async fn load_vaults(&mut self) -> Result<&[Summary]>;
+    async fn set_vault_name(
+        &mut self,
+        summary: &Summary,
+        name: &str,
+    ) -> Result<()>;
+    async fn open_vault(
+        &mut self,
+        summary: &Summary,
+        passphrase: &str,
+    ) -> Result<()>;
+    async fn reduce_wal(&mut self, summary: &Summary) -> Result<Vault>;
+    async fn patch(
+        &mut self,
+        summary: &Summary,
+        events: Vec<SyncEvent<'static>>,
+    ) -> Result<()>;
+    async fn handle_change(
+        &mut self,
+        change: ChangeNotification,
+    ) -> Result<(bool, HashSet<ChangeAction>)>;
+}
+*/
+
 /// Factory for creating providers.
 #[derive(Debug)]
 pub enum ProviderFactory {
+    /*
     /// Provider storing data in memory.
     Memory,
+    */
+
     /// Local provider using the default cache location.
     Local,
     /// Specific directory location.
@@ -54,7 +157,7 @@ impl fmt::Display for ProviderFactory {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
             Self::Local => write!(f, "local"),
-            Self::Memory => write!(f, "memory"),
+            //Self::Memory => write!(f, "memory"),
             Self::Directory(path) => write!(f, "{}", path.display()),
             Self::Remote(remote) => write!(f, "{}", remote),
         }
@@ -69,6 +172,7 @@ impl Default for ProviderFactory {
 
 impl ProviderFactory {
 
+    /*
     /// Create a provider.
     pub fn create_provider2(
         &self,
@@ -101,6 +205,7 @@ impl ProviderFactory {
             }
         }
     }
+    */
 
     /// Create a provider.
     pub fn create_provider(
@@ -122,7 +227,7 @@ impl ProviderFactory {
                 let dir = cache_dir().ok_or_else(|| Error::NoCache)?;
                 Ok(new_remote_file_provider(signer, dir, remote.clone())?)
             }
-            _ => todo!()
+            //_ => todo!()
         }
     }
 }
