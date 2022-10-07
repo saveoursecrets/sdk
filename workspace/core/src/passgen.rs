528977fe6702b095faa6ecc72718e3f30008cfd8
muji <muji@tmpfs.org>
2022-10-07 09:57:55 +0800

    Include score with generated passwords.
    Support generating multiple passwords as a batch.

--- workspace/core/src/passgen.rs
+++ workspace/core/src/passgen.rs
@@ -1,88 +1,100 @@
 //! Utility for generating random passwords.
-use rand::Rng;
 use crate::{Error, Result};
 use secrecy::SecretString;
 
-const MIN_LENGTH: u8 = 8;
-const ROMAN_LOWER: &str = "abcdefghijklmnopqrstuvwxyz";
-const ROMAN_UPPER: &str = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-const DIGITS: &str = "0123456789";
-const PUNCTUATION: &str = "!\"#$%&'()*+,-./:;<=>?@`~\\]^_{}";
+use passwords::{PasswordGenerator, analyzer, scorer};
+
+const MIN_LENGTH: usize = 8;
 
 /// Options for password generation.
 #[derive(Debug, Clone)]
 pub struct PassGenOptions {
-    length: u8,
-    characters: Vec<&'static str>,
+    inner: PasswordGenerator,
 }
 
 impl PassGenOptions {
+
+    /// Get the character length of the generated password.
+    pub fn len(&self) -> usize {
+        self.inner.length
+    }
+
     /// Options using uppercase and lowercase roman letters.
-    pub fn new_alpha(length: u8) -> Self {
+    pub fn new_alpha(length: usize) -> Self {
         Self {
-            length,
-            characters: vec![
-                ROMAN_LOWER,
-                ROMAN_UPPER,
-            ],
+            inner: PasswordGenerator::new()
+                .length(length)
+                .lowercase_letters(true)
+                .uppercase_letters(true)
+                .symbols(false)
         }
     }
 
     /// Options using numeric digits only.
-    pub fn new_numeric(length: u8) -> Self {
+    pub fn new_numeric(length: usize) -> Self {
         Self {
-            length,
-            characters: vec![
-                DIGITS,
-            ],
+            inner: PasswordGenerator::new()
+                .length(length)
+                .numbers(true)
+                .lowercase_letters(false)
+                .uppercase_letters(false)
+                .symbols(false)
         }
     }
 
     /// Options using numeric digits, uppercase and lowercase 
     /// roman letters.
-    pub fn new_alpha_numeric(length: u8) -> Self {
+    pub fn new_alpha_numeric(length: usize) -> Self {
         Self {
-            length,
-            characters: vec![
-                ROMAN_LOWER,
-                ROMAN_UPPER,
-                DIGITS,
-            ],
+            inner: PasswordGenerator::new()
+                .length(length)
+                .numbers(true)
+                .lowercase_letters(true)
+                .uppercase_letters(true)
+                .exclude_similar_characters(true)
         }
     }
 
     /// Options using printable ASCII characters.
-    pub fn new_ascii_printable(length: u8) -> Self {
+    pub fn new_ascii_printable(length: usize) -> Self {
         Self {
-            length,
-            characters: vec![
-                ROMAN_LOWER,
-                ROMAN_UPPER,
-                DIGITS,
-                PUNCTUATION,
-            ],
+            inner: PasswordGenerator::new()
+                .length(length)
+                .numbers(true)
+                .symbols(true)
+                .lowercase_letters(true)
+                .uppercase_letters(true)
+                .exclude_similar_characters(true)
         }
     }
 }
 
-/// Generate a random password.
-pub fn generate_password(options: PassGenOptions) -> Result<SecretString> {
-    if options.length < MIN_LENGTH {
+/// Generate a single random password.
+pub fn generate_one(options: PassGenOptions) -> Result<(SecretString, f64)> {
+    if options.len() < MIN_LENGTH {
         return Err(Error::PasswordLength(MIN_LENGTH));
     }
-    let rng = &mut rand::thread_rng();
-    let len = options.characters.iter().fold(0, |acc, s| acc + s.len());
-    let mut characters = Vec::with_capacity(len);
-    for chars in options.characters {
-        let mut list = chars.chars().collect();
-        characters.append(&mut list);
-    }
-    let mut password = String::new();
-    for _ in 0..options.length {
-        password.push(characters[rng.gen_range(0..len)]);
+
+    let password = options.inner.generate_one().map_err(|e| Error::PasswordGenerator(e.to_owned()))?;
+
+    let score = scorer::score(&analyzer::analyze(&password));
+
+    Ok((SecretString::new(password), score))
+}
+
+/// Generate multiple random passwords.
+pub fn generate(options: PassGenOptions, count: usize) -> Result<Vec<(SecretString, f64)>> {
+    if options.len() < MIN_LENGTH {
+        return Err(Error::PasswordLength(MIN_LENGTH));
     }
 
-    Ok(SecretString::new(password))
+    let passwords = options.inner.generate(count)
+        .map_err(|e| Error::PasswordGenerator(e.to_owned()))?;
+    Ok(passwords.into_iter().map(|password| {
+        let score = scorer::score(&analyzer::analyze(&password));
+        (SecretString::new(password), score)
+    }).collect())
+
 }
 
 #[cfg(test)]
@@ -94,47 +106,59 @@ mod test {
     #[test]
     fn passgen_invalid_length() -> Result<()> {
         let options = PassGenOptions::new_alpha(2);
-        assert!(generate_password(options).is_err());
+        assert!(generate_one(options).is_err());
         Ok(())
     }
 
     #[test]
     fn passgen_alpha() -> Result<()> {
         let options = PassGenOptions::new_alpha(12);
-        let password = generate_password(options.clone())?;
-        assert_eq!(options.length as usize, password.expose_secret().len());
+        let (password, _) = generate_one(options.clone())?;
+        assert_eq!(options.len(), password.expose_secret().len());
         Ok(())
     }
 
     #[test]
     fn passgen_numeric() -> Result<()> {
         let options = PassGenOptions::new_numeric(12);
-        let password = generate_password(options.clone())?;
-        assert_eq!(options.length as usize, password.expose_secret().len());
+        let (password, _) = generate_one(options.clone())?;
+        assert_eq!(options.len(), password.expose_secret().len());
         Ok(())
     }
 
     #[test]
     fn passgen_alphanumeric() -> Result<()> {
         let options = PassGenOptions::new_alpha_numeric(12);
-        let password = generate_password(options.clone())?;
-        assert_eq!(options.length as usize, password.expose_secret().len());
+        let (password, _) = generate_one(options.clone())?;
+        assert_eq!(options.len(), password.expose_secret().len());
         Ok(())
     }
 
     #[test]
     fn passgen_ascii_printable() -> Result<()> {
         let options = PassGenOptions::new_ascii_printable(12);
-        let password = generate_password(options.clone())?;
-        assert_eq!(options.length as usize, password.expose_secret().len());
+        let (password, _) = generate_one(options.clone())?;
+        assert_eq!(options.len(), password.expose_secret().len());
         Ok(())
     }
 
     #[test]
     fn passgen_ascii_printable_long() -> Result<()> {
         let options = PassGenOptions::new_ascii_printable(32);
-        let password = generate_password(options.clone())?;
-        assert_eq!(options.length as usize, password.expose_secret().len());
+        let (password, _) = generate_one(options.clone())?;
+        assert_eq!(options.len(), password.expose_secret().len());
+        Ok(())
+    }
+
+    #[test]
+    fn passgen_generate() -> Result<()> {
+        let options = PassGenOptions::new_ascii_printable(12);
+        let count = 5;
+        let passwords = generate(options.clone(), count)?;
+        assert_eq!(count, passwords.len());
+        for (password, _) in passwords {
+            assert_eq!(options.len(), password.expose_secret().len());
+        }
         Ok(())
     }
 }
