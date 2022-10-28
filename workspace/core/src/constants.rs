0325609926e0c0ac801217eff9d2ed2e81bfb511
muji <muji@tmpfs.org>
2022-10-28 13:05:03 +0800

    Remove indirection on encryption passphrase.
    By storing a different encryption passphrase in the login vault it would
    make a user vulnerable to a denial of service attack.
    
    If the login vault was deleted but the user still had files for the
    other vaults containing their user data they would not be able to access
    the secrets. By sharing the master vault password across all vaults we
    remove this attack vector.

--- workspace/core/src/constants.rs
+++ workspace/core/src/constants.rs
@@ -62,10 +62,6 @@ mod vault {
     /// Default public name for a vault.
     pub const DEFAULT_VAULT_NAME: &str = "Documents";
 
-    /// Login vault encryption passphrase name.
-    pub const LOGIN_ENCRYPTION_PASSPHRASE_NAME: &str =
-        "encryption-passphrase";
-
     /// Login vault signing key name.
     pub const LOGIN_SIGNING_KEY_NAME: &str = "signing-key";
 }
