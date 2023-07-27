014a7083b491cd6bb3669d8334224f03624ed8e6
muji <muji@tmpfs.org>
2023-07-27 17:11:06 +0800

    Add scripts to codesign and notarize (macos).

--- scripts/find-executables.sh
+++ scripts/find-executables.sh
@@ -4,14 +4,12 @@ set -e
 
 DIR="$1"
 GITHUB_ENV=${GITHUB_ENV:-/dev/null}
-
 OS="$(uname -s)"
+
 case "${OS}" in
   Darwin*) exes=$(find $DIR -maxdepth 1 -type f -perm +111 -print | xargs);;
   *) exes=$(find $DIR -maxdepth 1 -type f -executable -print | xargs)
 esac
 
 echo "EXES='${exes}'" >> $GITHUB_ENV
-
-#find $DIR -maxdepth 1 -type f -executable -print | xargs
-#find $DIR -maxdepth 1 -type f -perm +111 -print | xargs
+echo $exes
