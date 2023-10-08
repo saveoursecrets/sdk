9213c761338400b0720f9086736af90047215472
muji <muji@tmpfs.org>
2023-10-08 09:42:47 +0800

    Add types for releases information.

---
+++ workspace/artifact/src/artifact.rs
@@ -0,0 +1,198 @@
+//! Types for release artifact meta data.
+use std::{fmt, str::FromStr};
+use time::OffsetDateTime;
+use serde::{Serialize, Deserialize};
+use url::Url;
+use crate::Error;
+
+/// Artifact meta data represents a file.
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Artifact {
+    /// Distro channel name.
+    pub distro: Distro,
+    /// Processor architecture.
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub arch: Option<String>,
+    /// File name.
+    pub name: String,
+    /// Build version.
+    pub version: semver::Version,
+    /// Hash digest.
+    #[serde(with = "hex::serde")]
+    pub sha256: Vec<u8>,
+    /// Date and time.
+    pub timestamp: OffsetDateTime,
+    /// Commit hash.
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub commit: Option<String>,
+    /// File size of the artifact.
+    pub size: u64,
+    /// URL to a release artifact.
+    pub artifact: Option<Url>,
+    /// URL to the app on the Play or App store.
+    pub store: Option<Url>,
+}
+
+/// Distribution channel.
+#[derive(Debug, Clone, Hash, Eq, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "lowercase")]
+pub enum Channel {
+    /// Beta channel.
+    Beta,
+    /// Stable channel.
+    Stable,
+    /// Canary channel.
+    Canary,
+}
+
+impl fmt::Display for Channel {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "{}",
+            match self {
+                Self::Stable => "stable",
+                Self::Beta => "beta",
+                Self::Canary => "canary",
+            }
+        )
+    }
+}
+
+impl FromStr for Channel {
+    type Err = Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        Ok(match s {
+            "stable" => Self::Stable,
+            "beta" => Self::Beta,
+            _ => return Err(Error::UnknownChannel(s.to_owned())),
+        })
+    }
+}
+
+/// Collection of artifacts.
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(rename_all = "lowercase")]
+pub enum Collection {
+    /// Command line tools.
+    Cli,
+    /// Graphical user interfaces.
+    Gui,
+}
+
+impl fmt::Display for Collection {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "{}",
+            match self {
+                Self::Cli => "cli",
+                Self::Gui => "gui",
+            }
+        )
+    }
+}
+
+impl FromStr for Collection {
+    type Err = Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        Ok(match s {
+            "cli" => Self::Cli,
+            "gui" => Self::Gui,
+            _ => return Err(Error::UnknownCollection(s.to_owned())),
+        })
+    }
+}
+
+/// Distribution platform.
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(rename_all = "lowercase")]
+pub enum Distro {
+    /// Linux.
+    Linux,
+    /// Debian Linux.
+    Debian,
+    /// RedHat Linux (Fedora, CentOS etc).
+    RedHat,
+    /// Windows.
+    Windows,
+    /// MacOS.
+    MacOS,
+    /// iOS.
+    #[allow(non_camel_case_types)]
+    iOS,
+    /// Android.
+    Android,
+}
+
+impl fmt::Display for Distro {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "{}",
+            match self {
+                Self::Linux => "linux",
+                Self::Debian => "debian",
+                Self::RedHat => "redhat",
+                Self::Windows => "windows",
+                Self::MacOS => "macos",
+                Self::iOS => "ios",
+                Self::Android => "android",
+            }
+        )
+    }
+}
+
+impl FromStr for Distro {
+    type Err = Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        Ok(match s {
+            "linux" => Self::Linux,
+            "debian" => Self::Debian,
+            "redhat" => Self::RedHat,
+            "windows" => Self::Windows,
+            "macos" => Self::MacOS,
+            "ios" => Self::iOS,
+            "android" => Self::Android,
+            _ => return Err(Error::UnknownDistro(s.to_owned())),
+        })
+    }
+}
+
+/// Platform variant.
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(rename_all = "lowercase")]
+pub enum Variant {
+    /// No variant.
+    None,
+    /// MUSL.
+    Musl,
+}
+
+impl fmt::Display for Variant {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "{}",
+            match self {
+                Self::None => "none",
+                Self::Musl => "musl",
+            }
+        )
+    }
+}
+
+impl FromStr for Variant {
+    type Err = Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        Ok(match s {
+            "none" => Self::None,
+            "musl" => Self::Musl,
+            _ => return Err(Error::UnknownVariant(s.to_owned())),
+        })
+    }
+}
---
+++ workspace/artifact/src/release.rs
@@ -0,0 +1,118 @@
+//! Types for the public releases information.
+use serde::{Serialize, Deserialize};
+use std::collections::HashMap;
+use url::Url;
+use crate::artifact::{Channel, Artifact};
+
+/// Release information.
+#[derive(Serialize, Deserialize)]
+pub struct ReleaseInfo {
+    /// GUI release information.
+    pub gui: GuiReleaseInfo,
+}
+
+/// Release information for the GUI.
+#[derive(Serialize, Deserialize)]
+pub struct GuiReleaseInfo {
+    /// Release channels for the GUI.
+    #[serde(flatten)]
+    pub channels: HashMap<Channel, ChannelRelease>,
+}
+
+/// Release information for a single channel.
+#[derive(Serialize, Deserialize)]
+pub struct ChannelRelease {
+    /// Release artifact for MacOS.
+    pub macos: Artifact,
+    /// Release artifacts for Linux.
+    pub linux: LinuxRelease,
+    /// Release artifact for Windows.
+    pub windows: Artifact,
+    /// Release artifact for Android.
+    pub android: Artifact,
+    /// Release information for iOS.
+    pub ios: IosRelease,
+}
+
+/// Release information for the linux platform.
+#[derive(Serialize, Deserialize)]
+pub struct LinuxRelease {
+    /// Release information for debian distros.
+    pub debian: Artifact,
+}
+
+/// Release information for the iOS platform.
+#[derive(Serialize, Deserialize)]
+pub struct IosRelease {
+    /// URL to the app store installer.
+    pub store: Url,
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use anyhow::Result;
+
+    static JSON:&str = r#"{
+  "gui": {
+    "beta": {
+      "macos": {
+        "distro": "macos",
+        "name": "saveoursecrets.pkg",
+        "version": "1.0.0-beta1+83",
+        "sha256": "4276fff9125fd39ea1da6e5d97e06c6f840a5819dfd8a08af0a6519e3dd231f1",
+        "timestamp": "2023-10-07 07:17:27.64777 +00:00:00",
+        "commit": "cbae2ed875037917d74298e1e608baac984b5d95",
+        "size": 38517226,
+        "artifact": "https://releases.saveoursecrets.com/beta/gui/macos/1.0.0-beta1+83/saveoursecrets.pkg"
+      },
+      "linux": {
+        "debian": {
+          "distro": "debian",
+          "arch": "x86_64",
+          "name": "saveoursecrets.deb",
+          "version": "1.0.0-beta1+83",
+          "sha256": "4d56f4001fca4a490a5ebcce17d2ef1d1b76d0cccfa1797ad7853c944bbe0aa8",
+          "timestamp": "2023-10-07 07:16:01.308760682 +00:00:00",
+          "commit": "cbae2ed875037917d74298e1e608baac984b5d95",
+          "size": 17398354,
+          "artifact": "https://releases.saveoursecrets.com/beta/gui/debian/1.0.0-beta1+83/x86_64/saveoursecrets.deb"
+        }
+      },
+      "windows": {
+        "distro": "windows",
+        "arch": "x86_64",
+        "name": "saveoursecrets.msix",
+        "version": "1.0.0-beta1+83",
+        "sha256": "d443e491b92e191edf0d9b94b4fd9e6f1aa0d8f4464dd5e46248a1b34e083a2b",
+        "timestamp": "2023-10-07 07:30:04.9398342 +00:00:00",
+        "commit": "cbae2ed875037917d74298e1e608baac984b5d95",
+        "size": 22370419,
+        "artifact": "https://releases.saveoursecrets.com/beta/gui/windows/1.0.0-beta1+83/x86_64/saveoursecrets.msix"
+      },
+      "android": {
+        "distro": "android",
+        "name": "saveoursecrets.apk",
+        "version": "1.0.0-beta1+83",
+        "sha256": "8658a9783afb268d3acd0f9359d1d7e82276776f6e8ec7b7c6d7a778199a8bdb",
+        "timestamp": "2023-10-07 07:15:10.658351133 +00:00:00",
+        "commit": "cbae2ed875037917d74298e1e608baac984b5d95",
+        "size": 73603193,
+        "artifact": "https://releases.saveoursecrets.com/beta/gui/android/1.0.0-beta1+83/saveoursecrets.apk",
+        "store": "https://play.google.com/store/apps/details?id=com.saveoursecrets"
+      },
+      "ios": {
+        "store": "https://appstore.com/saveoursecrets"
+      }
+    }
+  }
+}"#;
+    
+    #[test]
+    fn test_serde() -> Result<()> {
+        let info: ReleaseInfo = serde_json::from_str(JSON)?;
+        let name = &info.gui.channels.get(&Channel::Beta).unwrap().macos.name;
+        assert_eq!("saveoursecrets.pkg", name);
+        Ok(())
+    }
+}
