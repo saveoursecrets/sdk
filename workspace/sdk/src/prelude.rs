7fb7b78dcfb3aca6e950d195beb4cdeaa4ced230
muji <muji@tmpfs.org>
2023-11-27 11:58:20 +0800

    Update prelude.

--- workspace/sdk/src/prelude.rs
+++ workspace/sdk/src/prelude.rs
@@ -1,10 +1,14 @@
 //! Prelude re-exports common types.
 pub use crate::account::*;
+pub use crate::account::archive::*;
+pub use crate::account::files::*;
+pub use crate::account::search::*;
 pub use crate::commit::*;
 pub use crate::constants::*;
 pub use crate::crypto::*;
 pub use crate::events::*;
 pub use crate::formats::*;
+pub use crate::search::*;
 pub use crate::signer::ecdsa::Address;
 pub use crate::vault::secret::*;
 pub use crate::vault::*;
