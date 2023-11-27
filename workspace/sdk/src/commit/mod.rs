fa2286c503a8857230faea7f329f0692119f7892
muji <muji@tmpfs.org>
2023-11-27 11:53:48 +0800

    Remove obsolete SyncInfo and SyncKind.

--- workspace/sdk/src/commit/mod.rs
+++ workspace/sdk/src/commit/mod.rs
@@ -15,32 +15,3 @@ pub use tree::{CommitTree, MultiTree};
 /// Commit state combines the last commit hash with
 /// a commit proof.
 pub type CommitState = (CommitHash, CommitProof);
-
-/// Enumerates the kind set after synchronization was attempted.
-#[derive(Serialize, Deserialize)]
-pub enum SyncKind {
-    /// Local and remote are equal.
-    Equal,
-    /// Safe synchronization was made.
-    Safe,
-    /// Forced synchronization was performed.
-    Force,
-    /// Synchronization is not safe and a
-    /// forceful attempt is required.
-    Unsafe,
-}
-
-/// Information yielded after attempting synchronization.
-#[derive(Serialize, Deserialize)]
-pub struct SyncInfo {
-    /// Local and remote proofs before synchronization.
-    pub before: (CommitProof, CommitProof),
-    /// Proof after synchronization.
-    ///
-    /// If the root hashes for local and remote are up to
-    /// date this will be `None`.
-    pub after: Option<CommitProof>,
-
-    /// The status of the synchronization attempt.
-    pub status: SyncKind,
-}
