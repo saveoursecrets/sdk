fdfc0601f11aeee0183f1806acad0db8361082eb
muji <muji@tmpfs.org>
2023-12-03 20:53:33 +0800

    Add test spec for file events on folder delete.

--- tests/integration/event_log/file_events.rs
+++ tests/integration/event_log/file_events.rs
@@ -1,3 +1,4 @@
+use super::all_events;
 use crate::test_utils::{mock, setup, teardown};
 use anyhow::Result;
 use sos_net::{
@@ -11,11 +12,11 @@ use sos_net::{
     },
 };
 
-const TEST_ID: &str = "events_file";
-
 /// Tests the various file events are being logged.
 #[tokio::test]
 async fn integration_events_file() -> Result<()> {
+    const TEST_ID: &str = "events_file";
+
     //crate::test_utils::init_tracing();
 
     let mut dirs = setup(TEST_ID, 1).await?;
@@ -118,6 +119,74 @@ async fn integration_events_file() -> Result<()> {
     ));
     assert!(matches!(
         events.get(4),
+        Some(FileEvent::DeleteFile(_, _, _))
+    ));
+
+    teardown(TEST_ID).await;
+
+    Ok(())
+}
+
+/// Tests that delete file events are logged when a folder
+/// is deleted.
+#[tokio::test]
+async fn integration_events_file_folder_delete() -> Result<()> {
+    const TEST_ID: &str = "events_file_folder_delete";
+
+    //crate::test_utils::init_tracing();
+
+    let mut dirs = setup(TEST_ID, 1).await?;
+    let data_dir = dirs.clients.remove(0);
+
+    let account_name = TEST_ID.to_string();
+    let (password, _) = generate_passphrase()?;
+
+    let (mut account, new_account) = LocalAccount::new_account(
+        account_name.clone(),
+        password.clone(),
+        Some(data_dir.clone()),
+        None,
+    )
+    .await?;
+
+    let default_folder = new_account.default_folder();
+    account.sign_in(password.clone()).await?;
+    account.open_folder(&default_folder).await?;
+
+    // Create some external file secrets
+    let (meta, secret, _) = mock::file_text_secret()?;
+    let (id, _, _, _) = account
+        .create_secret(meta, secret, Default::default())
+        .await?;
+    let (meta, secret, _) = mock::file_image_secret()?;
+    let (id, _, _, _) = account
+        .create_secret(meta, secret, Default::default())
+        .await?;
+
+    // Delete the folder
+    account.delete_folder(&default_folder).await?;
+
+    // Store the file events log so we can delete and re-create
+    let file_events = account.paths().file_events();
+
+    let mut event_log = FileEventLog::new_file(&file_events).await?;
+    let events = all_events(&mut event_log).await?;
+    assert_eq!(4, events.len());
+    assert!(matches!(
+        events.get(0),
+        Some(FileEvent::CreateFile(_, _, _))
+    ));
+    assert!(matches!(
+        events.get(1),
+        Some(FileEvent::CreateFile(_, _, _))
+    ));
+    // Both files were deleted
+    assert!(matches!(
+        events.get(2),
+        Some(FileEvent::DeleteFile(_, _, _))
+    ));
+    assert!(matches!(
+        events.get(3),
         Some(FileEvent::DeleteFile(_, _, _))
     ));
 
