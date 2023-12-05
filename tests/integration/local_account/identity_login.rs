c8cc793c57ec568c3c29835898c55bcfe8856dff
muji <muji@tmpfs.org>
2023-12-05 11:13:23 +0800

    Tweak identity login test spec.

--- tests/integration/local_account/identity_login.rs
+++ tests/integration/local_account/identity_login.rs
@@ -33,22 +33,16 @@ async fn integration_identity_login() -> Result<()> {
     let key: AccessKey = password.into();
     identity.login(&path, &key).await?;
 
-    let folder1 = VaultId::new_v4();
+    let folder = VaultId::new_v4();
     let access_key: AccessKey = identity.generate_folder_password()?.into();
     identity
-        .save_folder_password(&folder1, access_key.clone())
+        .save_folder_password(&folder, access_key.clone())
         .await?;
 
     assert_eq!(1, identity.secure_keys().len());
 
     // Should be able to find the password we saved
-    assert!(identity.find_folder_password(&folder1).await.is_ok());
-
-    /*
-    // Remove a folder password
-    identity.remove_folder_password(&folder1).await?;
-    assert_eq!(0, identity.secure_keys().len());
-    */
+    assert!(identity.find_folder_password(&folder).await.is_ok());
 
     identity.sign_out().await?;
 
@@ -57,6 +51,10 @@ async fn integration_identity_login() -> Result<()> {
     identity.login(&path, &key).await?;
 
     assert_eq!(1, identity.secure_keys().len());
+
+    // Remove the folder password
+    identity.remove_folder_password(&folder).await?;
+    assert_eq!(0, identity.secure_keys().len());
 
     teardown(TEST_ID).await;
 
