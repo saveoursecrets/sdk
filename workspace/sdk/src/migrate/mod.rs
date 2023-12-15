8c2cd983165496130fa964c2b18f3a78c5412216
muji <muji@tmpfs.org>
2023-12-15 16:31:46 +0800

    Tweak doc comment.

--- workspace/sdk/src/migrate/mod.rs
+++ workspace/sdk/src/migrate/mod.rs
@@ -1,10 +1,9 @@
 #![deny(missing_docs)]
 #![forbid(unsafe_code)]
 #![cfg_attr(all(doc, CHANNEL_NIGHTLY), feature(doc_auto_cfg))]
-//! Library to export secrets unencrypted for migration
-//! or to import unencrypted data.
+//! Export and import unencrypted data.
 //!
-//! Used to move between different software providers.
+//! Used to move between different apps.
 
 use async_trait::async_trait;
 
