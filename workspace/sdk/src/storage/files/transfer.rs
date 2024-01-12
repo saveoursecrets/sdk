e7d78e8baca164b2b7fb1ffc66baa75a5316a763
muji <muji@tmpfs.org>
2024-01-12 09:15:15 +0800

    Prepare FileTransfers logic.

--- workspace/sdk/src/storage/files/transfer.rs
+++ workspace/sdk/src/storage/files/transfer.rs
@@ -2,12 +2,13 @@
 use crate::{
     events::FileEvent,
     storage::files::{list_external_files, ExternalFile, FileMutationEvent},
+    sync::Client,
     vfs, Paths, Result,
 };
 use serde::{Deserialize, Serialize};
 use serde_with::{serde_as, DisplayFromStr};
-use std::{collections::HashMap, path::PathBuf};
-use tokio::sync::Mutex;
+use std::{collections::HashMap, path::PathBuf, sync::Arc};
+use tokio::sync::{Mutex, RwLock};
 
 /// Operations for file transfers.
 #[derive(Debug, Copy, Clone, Eq, PartialEq, Hash, Serialize, Deserialize)]
@@ -148,6 +149,80 @@ impl Transfers {
         let path = self.path.lock().await;
         let buffer = serde_json::to_vec_pretty(self)?;
         vfs::write(&*path, &buffer).await?;
+        Ok(())
+    }
+}
+
+/// Transfers files to multiple clients.
+///
+/// Reads operations from the queue, executes them on
+/// the list of clients and removes from the queue only
+/// when each operation has been completed on every client.
+pub struct FileTransfers;
+
+impl FileTransfers {
+    /// Spawn a task to transfer file operations.
+    pub fn start_transfer<E: Send + Sync + 'static>(
+        paths: Arc<Paths>,
+        queue: Arc<RwLock<Transfers>>,
+        clients: Vec<impl Client<Error = E> + Send + Sync + 'static>,
+    ) -> tokio::task::JoinHandle<std::result::Result<(), E>> {
+        tokio::task::spawn(async move {
+            loop {
+                let pending_operations = {
+                    let reader = queue.read().await;
+                    reader.queue.clone()
+                };
+
+                for (file, ops) in pending_operations {
+                    for op in ops {
+                        let results = Self::run_operation(
+                            Arc::clone(&paths), &file, &op, &clients).await?;
+
+                        todo!("check results indicate the operations are all completed...");
+                    }
+                }
+            }
+            Ok::<(), E>(())
+        })
+    }
+
+    async fn run_operation<E: Send + Sync + 'static>(
+        paths: Arc<Paths>,
+        file: &ExternalFile,
+        op: &TransferOperation,
+        clients: &Vec<impl Client<Error = E> + Send + Sync + 'static>,
+    ) -> std::result::Result<(), E> {
+        match op {
+            TransferOperation::Upload => {
+                let path = paths.file_location(
+                    file.vault_id(),
+                    file.secret_id(),
+                    file.file_name().to_string());
+                for client in clients {
+                    client.upload_file(&file, &path).await?;
+                }
+            }
+            TransferOperation::Download => {
+                let path = paths.file_location(
+                    file.vault_id(),
+                    file.secret_id(),
+                    file.file_name().to_string());
+                for client in clients {
+                    client.download_file(&file, &path).await?;
+                }
+            }
+            TransferOperation::Delete => {
+                for client in clients {
+                    client.delete_file(&file).await?;
+                }
+            }
+            TransferOperation::Move(dest) => {
+                for client in clients {
+                    client.move_file(&file, dest).await?;
+                }
+            }
+        }
         Ok(())
     }
 }
