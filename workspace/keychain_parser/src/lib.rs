d9e0f3d994d04d38ef9d23003efdcdb0e3e10270
muji <muji@tmpfs.org>
2024-02-09 13:37:16 +0800

    Extract to workspace crate.

---
+++ workspace/keychain_parser/src/lib.rs
@@ -0,0 +1,10 @@
+//! Parses the output of the `dump-keychain` command of `security(1)` on MacOS.
+
+mod error;
+mod parser;
+
+pub use error::Error;
+pub use parser::*;
+
+/// Result type for keychain parser.
+pub type Result<T> = std::result::Result<T, Error>;
