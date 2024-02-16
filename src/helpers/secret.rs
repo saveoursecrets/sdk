5d7100f1711696a0fefe5f127f5ffaa23be2637c
muji <muji@tmpfs.org>
2024-02-16 15:58:06 +0800

    Add newline to new secret message.

--- src/helpers/secret.rs
+++ src/helpers/secret.rs
@@ -242,6 +242,7 @@ fn multiline_banner(kind: &str, label: &str) {
     let banner = Banner::new()
         .padding(Padding::one())
         .text(format!("[{}] {}", kind, label).into())
+        .newline()
         .text(
             r#"To abort enter Ctrl+C
 To save enter Ctrl+D on a newline"#
