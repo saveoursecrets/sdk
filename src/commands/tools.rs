6cd21f93ff0410c46c3e03d22e8326daebf9a463
muji <muji@tmpfs.org>
2024-06-12 07:47:16 +0800

    Move convert cipher command to tools command.
    Closes #447.

--- src/commands/tools.rs
+++ src/commands/tools.rs
@@ -2,23 +2,40 @@ use crate::{
     commands::check::verify_events,
     helpers::{
         account::resolve_account,
-        messages::{fail, success},
+        account::resolve_user_with_password,
+        messages::{fail, info, success},
         readline::read_flag,
     },
     Error, Result,
 };
 use clap::Subcommand;
 use sos_net::sdk::{
+    account::Account,
     constants::EVENT_LOG_EXT,
+    crypto::{AccessKey, Cipher, KeyDerivation},
     encode,
     events::{FolderEventLog, FolderReducer},
     identity::AccountRef,
     vault::VaultId,
     vfs, Paths,
 };
+use terminal_banner::{Banner, Padding};
 
 #[derive(Subcommand, Debug)]
 pub enum Command {
+    /// Convert the cipher for an account.
+    ConvertCipher {
+        /// Account name or address.
+        #[clap(short, long)]
+        account: Option<AccountRef>,
+
+        /// Key derivation function.
+        #[clap(short, long)]
+        kdf: Option<KeyDerivation>,
+
+        /// Convert to this cipher.
+        cipher: Cipher,
+    },
     /// Repair a vault from a corresponding events file.
     RepairVault {
         /// Account name or address.
@@ -32,6 +49,50 @@ pub enum Command {
 /// Handle sync commands.
 pub async fn run(cmd: Command) -> Result<()> {
     match cmd {
+        Command::ConvertCipher {
+            account,
+            cipher,
+            kdf,
+        } => {
+            let (user, password) =
+                resolve_user_with_password(account.as_ref(), false).await?;
+            let mut owner = user.write().await;
+
+            let banner = Banner::new()
+                .padding(Padding::one())
+                .text("CONVERT CIPHER".into())
+                .newline()
+                .text(
+                  "Changing a cipher is a risky operation, be sure you understand the risks.".into())
+                .newline()
+                .text(
+                  "* Vault and event logs will be overwritten".into())
+                .text(
+                  "* Event history is compacted".into())
+                .text(
+                  "* Sync will overwrite data on servers".into());
+
+            let result = banner.render();
+            println!("{}", result);
+
+            let prompt =
+                format!(r#"Convert to cipher "{}" (y/n)? "#, &cipher);
+            if read_flag(Some(&prompt))? {
+                let access_key: AccessKey = password.into();
+                let conversion = owner
+                    .change_cipher(&access_key, &cipher, kdf.clone())
+                    .await?;
+                if conversion.is_empty() {
+                    info(format!(
+                        "no files to convert, all folders use {} and {}",
+                        cipher,
+                        kdf.unwrap_or_default(),
+                    ));
+                } else {
+                    success("cipher changed");
+                }
+            }
+        }
         Command::RepairVault { account, folder } => {
             let account = resolve_account(Some(&account))
                 .await
