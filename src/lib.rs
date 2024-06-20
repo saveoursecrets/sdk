5fbf6adae02f39c34246e491cf81e532ebea1a93
muji <muji@tmpfs.org>
2024-06-20 11:47:45 +0800

    Update doc comments.

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,7 @@
 //! Command line tools for [Save Our Secrets](https://saveoursecrets.com).
 //!
-//! This crate contains the binaries for the `sos` client and
-//! the `sos-server` reference server; more information is on the
-//! [command line tools](https://saveoursecrets.com/command-line-tools/) downloads page.
+//! This crate contains the binary for the `sos`; more information is on the
+//! [command line tools](https://saveoursecrets.com/command-line-tools/) downloads page; for the server binary see the [sos-server](https://docs.rs/sos-server/) crate.
 //!
 //! See the [CLI documentation](https://saveoursecrets.com/docs/cli/) for usage information or browse the [online help manual](https://saveoursecrets.com/docs/cli/help/); the libraries are available at [sos-sdk](https://docs.rs/sos-sdk/) and [sos-net](https://docs.rs/sos-net/).
 #![deny(missing_docs)]
