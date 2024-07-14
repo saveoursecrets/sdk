163f559af5583b897957da1b764216be125354c9
muji <muji@tmpfs.org>
2024-07-14 12:53:47 +0800

    Move commands to tools command.

--- crates/sos/src/commands/mod.rs
+++ crates/sos/src/commands/mod.rs
@@ -1,25 +1,18 @@
 pub mod account;
-pub mod audit;
-pub mod check;
 pub mod device;
 pub mod environment;
-pub mod events;
 // pub mod file;
 pub mod folder;
 pub mod preferences;
 pub mod secret;
-pub mod security_report;
 pub mod server;
 pub mod shell;
 pub mod sync;
 pub mod tools;
 
 pub use account::Command as AccountCommand;
-pub use audit::Command as AuditCommand;
-pub use check::Command as CheckCommand;
 pub use device::Command as DeviceCommand;
 pub use environment::Command as EnvironmentCommand;
-pub use events::Command as EventsCommand;
 // pub use file::Command as FileCommand;
 pub use folder::Command as FolderCommand;
 pub use preferences::Command as PreferenceCommand;
