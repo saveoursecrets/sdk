3550f3d07b7ac03b8a55a78cef2d0a56554d03f9
muji <muji@tmpfs.org>
2024-10-04 22:18:56 +0800

    Update rustc_version, fix lint warning.

--- crates/protocol/build.rs
+++ crates/protocol/build.rs
@@ -2,6 +2,8 @@ extern crate prost_build;
 use rustc_version::{version_meta, Channel};
 
 fn main() {
+    println!("cargo::rustc-check-cfg=cfg(CHANNEL_NIGHTLY)");
+
     // Set cfg flags depending on release channel
     let channel = match version_meta().unwrap().channel {
         Channel::Stable => "CHANNEL_STABLE",
