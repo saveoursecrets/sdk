15cfab134bf95eb0572b6db792bf387a40cc6e3f
muji <muji@tmpfs.org>
2024-11-02 07:53:55 +0800

    Prepare helper function to send to native bridge.

--- crates/ipc/src/native_bridge.rs
+++ crates/ipc/src/native_bridge.rs
@@ -4,7 +4,7 @@
 //! Used to support the native messaging API provided
 //! by browser extensions.
 
-use crate::{IpcRequest, Result};
+use crate::{IpcRequest, IpcResponse, Result};
 use futures_util::{SinkExt, StreamExt};
 use sos_net::sdk::{logs::Logger, prelude::IPC_GUI_SOCKET_NAME};
 use tokio_util::codec::LengthDelimitedCodec;
@@ -76,4 +76,42 @@ pub async fn run(options: NativeBridgeOptions) -> Result<()> {
     }
 
     Ok(())
+}
+
+/// Send a request to a native bridge executable.
+pub async fn send<S: AsRef<std::ffi::OsStr>>(
+    command: S,
+    extension_id: String,
+    request: &IpcRequest,
+) -> Result<IpcResponse> {
+    use std::process::Stdio;
+    use tokio::process::Command;
+
+    let mut child = Command::new(command)
+        .arg(extension_id)
+        .stdout(Stdio::piped())
+        .stdin(Stdio::piped())
+        .spawn()?;
+
+    let stdout = child.stdout.take().unwrap();
+    let stdin = child.stdin.take().unwrap();
+
+    let mut stdin = LengthDelimitedCodec::builder()
+        .native_endian()
+        .new_write(stdin);
+
+    let mut stdout = LengthDelimitedCodec::builder()
+        .native_endian()
+        .new_read(stdout);
+
+    let message = serde_json::to_vec(request)?;
+    stdin.send(message.into()).await?;
+
+    while let Some(response) = stdout.next().await {
+        let response = response?;
+        let response: IpcResponse = serde_json::from_slice(&response)?;
+        return Ok(response);
+    }
+
+    unreachable!();
 }
