7db973b417ae40f8594a87a280b8de62b4373ab2
muji <muji@tmpfs.org>
2024-11-14 13:02:10 +0800

    Update typescript definitions.

--- packages/types/preamble.ts
+++ packages/types/preamble.ts
@@ -2,5 +2,6 @@ export type TupleOfTwo<T, U> = [T, U];
 export type AccountState = TupleOfTwo<PublicIdentity, boolean>;
 export type AccountsList = AccountState[];
 export type DocumentsList = Document[];
-export type SearchResults = TupleOfTwo<PublicIdentity, DocumentsList>;
+export type SearchResultEntry = TupleOfTwo<PublicIdentity, DocumentsList>;
+export type SearchResults = SearchResultEntry[];
 export type HashSet<T> = Set<T>;
