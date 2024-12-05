9950d87941621d984fb1776a720b83bbfbb3d447
muji <muji@tmpfs.org>
2024-12-06 05:38:04 +0800

    Delete obsolete ipc tests.

--- crates/integration_tests/tests/ipc/main.rs
+++ crates/integration_tests/tests/ipc/main.rs
@@ -1,11 +1,6 @@
 mod app_info;
-// mod authenticate_success;
-// mod authenticate_timeout;
 mod list_accounts;
 mod local_sync;
-// mod lock_account;
-// mod ping_pong;
-// mod search;
 pub use sos_test_utils as test_utils;
 
 use async_trait::async_trait;
