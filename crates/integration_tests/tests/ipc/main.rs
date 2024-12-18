95b77652fb50ca6eda774f895b13197a114aef07
muji <muji@tmpfs.org>
2024-12-18 15:00:59 +0800

    Tweak comment.

--- crates/integration_tests/tests/ipc/main.rs
+++ crates/integration_tests/tests/ipc/main.rs
@@ -1,8 +1,8 @@
-mod memory_server;
 mod extension_helper_chunks;
 mod extension_helper_info;
 mod extension_helper_list_accounts;
 mod extension_helper_probe;
+mod memory_server;
 
 pub use sos_test_utils as test_utils;
 
@@ -17,7 +17,7 @@ pub fn extension_helper_cmd<'a>(
         "test-extension-helper",
         "--",
         "sos-test-extension-helper", // mock extension name
-        data_dir,                 // data directory for isolated tests
+        data_dir,                    // data directory for isolated tests
     ];
     (command, arguments)
 }
