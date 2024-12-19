a4365a3c339f912269037032d06ede57984cb3e4
muji <muji@tmpfs.org>
2024-12-19 15:27:37 +0800

    Restore some tests for windows.

--- crates/integration_tests/tests/network_account/change_folder_password.rs
+++ crates/integration_tests/tests/network_account/change_folder_password.rs
@@ -7,7 +7,6 @@ use sos_net::{protocol::AccountSync, sdk::prelude::*};
 /// Tests changing a folder password and force syncing
 /// the updated folder events log.
 #[tokio::test]
-#[cfg_attr(windows, ignore = "fix auto lock bug on windows (#451)")]
 async fn network_sync_change_folder_password() -> Result<()> {
     const TEST_ID: &str = "sync_change_folder_password";
     // crate::test_utils::init_tracing();
