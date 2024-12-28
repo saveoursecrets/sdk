cc5c8a2ef562ee1270e9f61f560997e2be4287fa
muji <muji@tmpfs.org>
2024-12-28 17:00:29 +0800

    Prepare test spec.

--- crates/integration_tests/tests/database/importer.rs
+++ crates/integration_tests/tests/database/importer.rs
@@ -1,5 +1,10 @@
-use crate::test_utils::setup;
+use crate::test_utils::{setup, teardown};
 use anyhow::Result;
+use sos_sdk::prelude::{
+    vfs, Account, AccountBackup, Inventory, LocalAccount, Paths,
+    RestoreOptions,
+};
+use tokio::io::BufReader;
 
 #[tokio::test]
 async fn database_importer() -> Result<()> {
@@ -7,6 +12,28 @@ async fn database_importer() -> Result<()> {
 
     let mut dirs = setup(TEST_ID, 1).await?;
     let data_dir = dirs.clients.remove(0);
+    Paths::scaffold(Some(data_dir.clone())).await?;
+
+    // Prepare to import to database file
+    // by restoring an account from a backup archive fixture
+    let archive =
+        "../../fixtures/backups/v1/0xba0faea9bbc182e3f4fdb3eea7636b5bb31ea9ac.zip";
+    let reader = vfs::File::open(archive).await?;
+    let inventory: Inventory =
+        AccountBackup::restore_archive_inventory(BufReader::new(reader))
+            .await?;
+    let options = RestoreOptions {
+        selected: inventory.vaults,
+        ..Default::default()
+    };
+    LocalAccount::import_backup_archive(
+        &archive,
+        options,
+        Some(data_dir.clone()),
+    )
+    .await?;
+
+    teardown(TEST_ID).await;
 
     Ok(())
 }
