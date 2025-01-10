585376e181809897fc7bae47ab080d27501b94b2
muji <muji@tmpfs.org>
2025-01-10 20:45:15 +0800

    Tidy backend folder implementation.

--- crates/backend/src/folder.rs
+++ crates/backend/src/folder.rs
@@ -1,4 +1,8 @@
-//! Folder implementation combining a access point with an event log.
+//! Folder combines an access point with an event log.
+use crate::event_log::BackendEventLog;
+use crate::reducers::FolderReducer;
+use crate::Error;
+use crate::FolderEventLog as BackendFolderEventLog;
 use crate::{AccessPoint, FolderEventLog, Result};
 use sos_core::{
     commit::{CommitHash, CommitState},
@@ -7,10 +11,18 @@ use sos_core::{
     events::{EventRecord, ReadEvent, WriteEvent},
     VaultFlags,
 };
+use sos_core::{constants::EVENT_LOG_EXT, decode};
+use sos_filesystem::{
+    events::FolderEventLog as FsFolderEventLog, FileSystemAccessPoint,
+    VaultFileWriter,
+};
+use sos_vault::Vault;
 use sos_vault::{
     secret::{Secret, SecretId, SecretMeta, SecretRow},
     SecretAccess, VaultCommit, VaultId, VaultMeta,
 };
+use sos_vfs as vfs;
+use std::path::Path;
 use std::{borrow::Cow, sync::Arc};
 use tokio::sync::RwLock;
 
@@ -21,8 +33,62 @@ pub struct Folder {
 }
 
 impl Folder {
+    /// Create a new folder from a vault file on disc.
+    ///
+    /// Changes to the in-memory vault are mirrored to disc and
+    /// and if an event log does not exist it is created.
+    pub async fn new_fs(path: impl AsRef<Path>) -> Result<Self> {
+        let mut events_path = path.as_ref().to_owned();
+        events_path.set_extension(EVENT_LOG_EXT);
+
+        let mut event_log =
+            FsFolderEventLog::<Error>::new(events_path).await?;
+        event_log.load_tree().await?;
+        let needs_init = event_log.tree().root().is_none();
+
+        let vault = if needs_init {
+            // For the client-side we must split the events
+            // out but keep the existing vault data (not the head-only)
+            // version so that the event log here will match what the
+            // server will have when an account is first synced
+            let buffer = vfs::read(path.as_ref()).await?;
+            let vault: Vault = decode(&buffer).await?;
+            let (_, events) =
+                FolderReducer::split::<Error>(vault.clone()).await?;
+            event_log.apply(events.iter().collect()).await?;
+            vault
+        } else {
+            let buffer = vfs::read(path.as_ref()).await?;
+            let vault: Vault = decode(&buffer).await?;
+            vault
+        };
+
+        let mirror = VaultFileWriter::<Error>::new(path.as_ref()).await?;
+        let keeper = FileSystemAccessPoint::<Error>::new_mirror(
+            vault,
+            Box::new(mirror),
+        );
+
+        Ok(Self::init(
+            AccessPoint::FileSystem(keeper),
+            BackendEventLog::FileSystem(event_log),
+        ))
+    }
+
+    /// Load a folder event log from the given path.
+    pub async fn new_fs_event_log(
+        path: impl AsRef<Path>,
+    ) -> Result<Arc<RwLock<BackendFolderEventLog>>> {
+        let mut event_log =
+            FsFolderEventLog::<Error>::new(path.as_ref().to_owned()).await?;
+        event_log.load_tree().await?;
+        Ok(Arc::new(RwLock::new(BackendFolderEventLog::FileSystem(
+            event_log,
+        ))))
+    }
+
     /// Create a new folder.
-    pub(super) fn init(keeper: AccessPoint, events: FolderEventLog) -> Self {
+    fn init(keeper: AccessPoint, events: FolderEventLog) -> Self {
         Self {
             keeper,
             events: Arc::new(RwLock::new(events)),
@@ -206,5 +272,11 @@ impl Folder {
         let mut event_log = self.events.write().await;
         event_log.clear().await?;
         Ok(())
+    }
+}
+
+impl From<Folder> for Vault {
+    fn from(value: Folder) -> Self {
+        value.keeper.into()
     }
 }
