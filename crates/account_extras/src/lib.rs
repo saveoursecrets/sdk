47ecc01659bf60b7965247c9701a4509dab777f2
muji <muji@tmpfs.org>
2025-01-16 16:26:48 +0800

    Remove obsolete sos-account-extras crate.

--- crates/account_extras/src/lib.rs
+++
@@ -1,5 +0,0 @@
-//! Extra functions for [local accounts](https://docs.rs/sos-account/latest/sos_account/) in the [Save Our Secrets](https://saveoursecrets.com) SDK.
-//!
-//! ***Deprecated***: use `sos-preferences`, `sos-system-messages` and `sos-security-report` instead.
-#![deny(missing_docs)]
-#![forbid(unsafe_code)]
