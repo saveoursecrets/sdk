34b0ed12cd8603cb85c90d85a5c0a5bf63f40e95
muji <muji@tmpfs.org>
2025-02-24 16:17:04 +0800

    Update event log handling in CLI.
    Improve to also add support for the login folder events and JSON output.

--- crates/sos/src/commands/tools/events.rs
+++ crates/sos/src/commands/tools/events.rs
@@ -1,19 +1,28 @@
-use crate::{Error, Result};
+use crate::{helpers::account::resolve_account_address, Error, Result};
 use binary_stream::futures::{Decodable, Encodable};
 use clap::Subcommand;
 use futures::{pin_mut, stream::StreamExt};
+use serde::Serialize;
 use sos_backend::{
-    AccountEventLog, BackendEventLog, DeviceEventLog, FileEventLog,
-    FolderEventLog,
+    AccountEventLog, BackendEventLog, BackendTarget, DeviceEventLog,
+    FileEventLog, FolderEventLog,
 };
 use sos_core::{
     commit::{CommitHash, CommitTree},
     events::{
-        AccountEvent, DeviceEvent, EventLog, FileEvent, LogEvent, WriteEvent,
+        AccountEvent, DeviceEvent, EventKind, EventLog, FileEvent, LogEvent,
+        WriteEvent,
     },
+    AccountRef, FolderRef, Paths, UtcDateTime,
 };
-use sos_vfs as vfs;
-use std::path::PathBuf;
+
+#[derive(Debug, Serialize)]
+struct EventLogDump {
+    version: u16,
+    length: usize,
+    root: CommitHash,
+    events: Vec<(EventKind, CommitHash, UtcDateTime)>,
+}
 
 #[derive(Subcommand, Debug)]
 pub enum Command {
@@ -23,8 +32,25 @@ pub enum Command {
         #[clap(short, long)]
         until_commit: Option<CommitHash>,
 
-        /// Log file path.
-        file: PathBuf,
+        /// Print as JSON.
+        #[clap(short, long)]
+        json: bool,
+
+        /// Account name or identifier.
+        account: AccountRef,
+    },
+    /// Print login folder event log records.
+    Login {
+        /// Iterate upto and including a specific commit.
+        #[clap(short, long)]
+        until_commit: Option<CommitHash>,
+
+        /// Print as JSON.
+        #[clap(short, long)]
+        json: bool,
+
+        /// Account name or identifier.
+        account: AccountRef,
     },
     /// Print device event log records.
     Device {
@@ -32,8 +58,12 @@ pub enum Command {
         #[clap(short, long)]
         until_commit: Option<CommitHash>,
 
-        /// Log file path.
-        file: PathBuf,
+        /// Print as JSON.
+        #[clap(short, long)]
+        json: bool,
+
+        /// Account name or identifier.
+        account: AccountRef,
     },
     /// Print folder event log records.
     Folder {
@@ -41,8 +71,15 @@ pub enum Command {
         #[clap(short, long)]
         until_commit: Option<CommitHash>,
 
-        /// Log file path.
-        file: PathBuf,
+        /// Print as JSON.
+        #[clap(short, long)]
+        json: bool,
+
+        /// Account name or identifier.
+        account: AccountRef,
+
+        /// Folder name or identifier.
+        folder: FolderRef,
     },
     /// Print file event log records.
     File {
@@ -50,51 +87,88 @@ pub enum Command {
         #[clap(short, long)]
         until_commit: Option<CommitHash>,
 
-        /// Log file path.
-        file: PathBuf,
+        /// Print as JSON.
+        #[clap(short, long)]
+        json: bool,
+
+        /// Account name or identifier.
+        account: AccountRef,
     },
 }
 
 pub async fn run(cmd: Command) -> Result<()> {
     match cmd {
-        Command::Account { file, until_commit } => {
-            if !vfs::metadata(&file).await?.is_file() {
-                return Err(Error::NotFile(file));
-            }
-
-            /*
-            let event_log = AccountEventLog::new_fs_account(&file).await?;
-            print_events::<AccountEvent>(event_log, until_commit).await?;
-            */
-
-            todo!("restore CLI account events");
+        Command::Account {
+            account,
+            json,
+            until_commit,
+        } => {
+            let account_id = resolve_account_address(Some(&account)).await?;
+            let paths = Paths::new_client(Paths::data_dir()?);
+            let target = BackendTarget::from_paths(&paths).await?;
+            let event_log =
+                AccountEventLog::new_account(target, &account_id).await?;
+            print_events::<AccountEvent>(event_log, json, until_commit)
+                .await?;
         }
-        Command::Device { file, until_commit } => {
-            /*
-            if !vfs::metadata(&file).await?.is_file() {
-                return Err(Error::NotFile(file));
-            }
-            let event_log = DeviceEventLog::new_fs_device(&file).await?;
-            print_events::<DeviceEvent>(event_log, until_commit).await?;
-            */
-            todo!("restore CLI device events");
+        Command::Login {
+            account,
+            json,
+            until_commit,
+        } => {
+            let account_id = resolve_account_address(Some(&account)).await?;
+            let paths = Paths::new_client(Paths::data_dir()?);
+            let target = BackendTarget::from_paths(&paths).await?;
+            let event_log =
+                FolderEventLog::new_login_folder(target, &account_id).await?;
+            print_events::<WriteEvent>(event_log, json, until_commit).await?;
         }
-        Command::Folder { file, until_commit } => {
-            if !vfs::metadata(&file).await?.is_file() {
-                return Err(Error::NotFile(file));
-            }
-            let event_log = FolderEventLog::new_fs_folder(&file).await?;
-            print_events::<WriteEvent>(event_log, until_commit).await?;
+        Command::Device {
+            account,
+            json,
+            until_commit,
+        } => {
+            let account_id = resolve_account_address(Some(&account)).await?;
+            let paths = Paths::new_client(Paths::data_dir()?);
+            let target = BackendTarget::from_paths(&paths).await?;
+            let event_log =
+                DeviceEventLog::new_device(target, &account_id).await?;
+            print_events::<DeviceEvent>(event_log, json, until_commit)
+                .await?;
         }
-        Command::File { file, until_commit } => {
-            /*
-            if !vfs::metadata(&file).await?.is_file() {
-                return Err(Error::NotFile(file));
-            }
-            let event_log = FileEventLog::new_fs_file(&file).await?;
-            print_events::<FileEvent>(event_log, until_commit).await?;
-            */
-            todo!("restore CLI file events");
+        Command::Folder {
+            account,
+            folder,
+            json,
+            until_commit,
+        } => {
+            let account_id = resolve_account_address(Some(&account)).await?;
+            let paths = Paths::new_client(Paths::data_dir()?);
+            let target = BackendTarget::from_paths(&paths).await?;
+            let folders = target.list_folders(&account_id).await?;
+            let folder = folders
+                .iter()
+                .find(|f| match &folder {
+                    FolderRef::Id(id) => f.id() == id,
+                    FolderRef::Name(name) => f.name() == name,
+                })
+                .ok_or_else(|| Error::FolderNotFound(folder.to_string()))?;
+            let event_log =
+                FolderEventLog::new_folder(target, &account_id, folder.id())
+                    .await?;
+            print_events::<WriteEvent>(event_log, json, until_commit).await?;
+        }
+        Command::File {
+            account,
+            json,
+            until_commit,
+        } => {
+            let account_id = resolve_account_address(Some(&account)).await?;
+            let paths = Paths::new_client(Paths::data_dir()?);
+            let target = BackendTarget::from_paths(&paths).await?;
+            let event_log =
+                FileEventLog::new_file(target, &account_id).await?;
+            print_events::<FileEvent>(event_log, json, until_commit).await?;
         }
     }
 
@@ -106,6 +180,7 @@ async fn print_events<
     T: Default + Encodable + Decodable + LogEvent + Send + Sync + 'static,
 >(
     event_log: BackendEventLog<T>,
+    json: bool,
     until_commit: Option<CommitHash>,
 ) -> Result<()> {
     let version = event_log.version();
@@ -115,15 +190,23 @@ async fn print_events<
     pin_mut!(stream);
 
     let mut tree = CommitTree::new();
+    let mut events = Vec::new();
 
     while let Some(event) = stream.next().await {
         let (record, event) = event?;
 
-        println!("{}", divider);
-        println!("   time: {}", record.time());
-        println!(" before: {}", record.last_commit());
-        println!(" commit: {}", record.commit());
-        println!("  event: {}", event.event_kind());
+        if !json {
+            println!("{}", divider);
+            println!("   time: {}", record.time());
+            println!(" commit: {}", record.commit());
+            println!("  event: {}", event.event_kind());
+        } else {
+            events.push((
+                event.event_kind(),
+                record.commit().clone(),
+                record.time().clone(),
+            ));
+        }
 
         tree.append(&mut vec![record.commit().into()]);
         tree.commit();
@@ -137,11 +220,22 @@ async fn print_events<
 
     if tree.len() > 0 {
         let root = tree.root().unwrap();
-        println!("{}", divider);
-        println!("   root: {}", root);
-        println!("  total: {}", tree.len());
-        println!("version: {}", version);
-        println!("{}", divider);
+
+        if !json {
+            println!("{}", divider);
+            println!("   root: {}", root);
+            println!("  total: {}", tree.len());
+            println!("version: {}", version);
+            println!("{}", divider);
+        } else {
+            let data = EventLogDump {
+                version,
+                length: events.len(),
+                root,
+                events,
+            };
+            serde_json::to_writer_pretty(std::io::stdout(), &data)?;
+        }
     } else {
         println!("No events yet");
     }
