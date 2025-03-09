70db3e961762fdd444e7ac298aa5fdd21d497857
muji <muji@tmpfs.org>
2025-03-09 15:27:08 +0800

    Update features and tokio::main.

--- crates/extension_service/src/main.rs
+++ crates/extension_service/src/main.rs
@@ -1,7 +1,7 @@
 /// Forwards Native Messaging API JSON requests to an
 /// in-memory web service.
 #[doc(hidden)]
-#[tokio::main]
+#[tokio::main(flavor = "current_thread")]
 pub async fn main() -> anyhow::Result<()> {
     sos_extension_service::run().await
 }
