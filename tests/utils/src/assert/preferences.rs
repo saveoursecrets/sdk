bf35c22e300afa82ccfbe0bd5c8ce5bf8414a508
muji <muji@tmpfs.org>
2025-11-23 15:04:33 +0800

    Improve preferences test spec for new Json variant.

--- tests/utils/src/assert/preferences.rs
+++ tests/utils/src/assert/preferences.rs
@@ -1,4 +1,7 @@
+use std::{collections::HashMap, net::SocketAddr};
+
 use sos_preferences::{Preference, Preferences};
+use sos_protocol::network_client::NetworkConfig;
 
 /// Assert on the operations for a preferences collection.
 pub async fn assert_preferences<E>(
@@ -32,6 +35,20 @@ where
     // Update a preferenece
     prefs.insert("mock.bool".to_owned(), false.into()).await?;
 
+    let mut complex = HashMap::new();
+    complex.insert("foo".to_string(), "bar".to_string());
+    let complex = serde_json::to_value(&complex)?;
+    prefs.insert("mock.map".to_owned(), complex.into()).await?;
+
+    let mut net_config = NetworkConfig::default();
+    net_config
+        .resolve_addrs
+        .insert("foo.local".to_owned(), "192.168.1.33:8080".parse()?);
+    let net_config = serde_json::to_value(&net_config)?;
+    prefs
+        .insert("mock.network.config".to_owned(), net_config.into())
+        .await?;
+
     // Retrieve preferences
     let missing = prefs.get_unchecked("mock.non-existent");
     assert!(missing.is_none());
@@ -50,6 +67,21 @@ where
 
     let string_list = prefs.get_string_list("mock.string-list")?;
     assert!(matches!(string_list, Some(Preference::StringList(_))));
+
+    let json_value = prefs.get_json_value("mock.map")?;
+    assert!(matches!(json_value, Some(Preference::Json(_))));
+
+    let net_config = prefs.get_json_value("mock.network.config")?;
+
+    if let Some(Preference::Json(val)) = net_config {
+        let net_config: NetworkConfig = serde_json::from_value(val.clone())?;
+        assert_eq!(
+            "192.168.1.33:8080".parse::<SocketAddr>().ok().as_ref(),
+            net_config.resolve_addrs.get("foo.local")
+        );
+    } else {
+        panic!("expecting JSON preference");
+    }
 
     // Remove preferences
     let removed = prefs.remove("mock.bool").await?;
