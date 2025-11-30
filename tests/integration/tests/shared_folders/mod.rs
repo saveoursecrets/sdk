65e37b1dd6c8e39e2b66b4847e83c3eb4b88d78c
muji <muji@tmpfs.org>
2025-11-30 09:11:49 +0800

    Prepare stub test spec.

--- tests/integration/tests/shared_folders/mod.rs
+++ tests/integration/tests/shared_folders/mod.rs
@@ -1,1 +1,66 @@
+use anyhow::Result;
+use secrecy::{ExposeSecret, SecretString};
+use sos_account::{Account, SecretChange};
+use sos_core::{crypto::AccessKey, VaultId};
+use sos_net::NetworkAccount;
+use sos_test_utils::mock;
+use sos_vault::secret::Secret;
+
 mod shared_folder_secret_lifecycle;
+mod shared_folder_write_access;
+
+pub async fn assert_shared_folder_lifecycle(
+    owner: &mut NetworkAccount,
+    folder_id: &VaultId,
+    password: SecretString,
+    test_id: &str,
+) -> Result<()> {
+    let (meta, secret) = mock::note(test_id, test_id);
+    let SecretChange { id: secret_id, .. } =
+        owner.create_secret(meta, secret, folder_id.into()).await?;
+
+    let (row, _) = owner.read_secret(&secret_id, Some(folder_id)).await?;
+
+    assert!(matches!(row.secret(), Secret::Note { .. }));
+
+    let new_value = "<new value>";
+    let (_, meta, _) = row.into();
+    owner
+        .update_secret(
+            &secret_id,
+            meta,
+            Some(Secret::Note {
+                text: new_value.into(),
+                user_data: Default::default(),
+            }),
+            folder_id.into(),
+        )
+        .await?;
+
+    // Sign out and then re-authenticate to check
+    // shared folder access from sign in
+
+    owner.sign_out().await?;
+
+    let key: AccessKey = password.into();
+    owner.sign_in(&key).await?;
+
+    // Check we can read a secret created in the previous session
+    let (row, _) = owner.read_secret(&secret_id, Some(folder_id)).await?;
+    let value = if let Secret::Note { text, .. } = row.secret() {
+        text.expose_secret().to_owned()
+    } else {
+        panic!("expecting a secret note");
+    };
+    assert_eq!(new_value, &value);
+
+    // Create another secret
+    let (meta, secret) = mock::note(test_id, test_id);
+    let SecretChange { id: secret_id, .. } =
+        owner.create_secret(meta, secret, folder_id.into()).await?;
+
+    // Check secret deletion
+    owner.delete_secret(&secret_id, folder_id.into()).await?;
+
+    Ok(())
+}
--- tests/integration/tests/shared_folders/shared_folder_secret_lifecycle.rs
+++ tests/integration/tests/shared_folders/shared_folder_secret_lifecycle.rs
@@ -1,9 +1,7 @@
 use anyhow::Result;
-use secrecy::ExposeSecret;
-use sos_account::{Account, SecretChange};
+use sos_account::Account;
 use sos_client_storage::NewFolderOptions;
-use sos_sdk::prelude::*;
-use sos_test_utils::{mock, simulate_device, spawn, teardown};
+use sos_test_utils::{simulate_device, spawn, teardown};
 
 /// Tests creating a shared folder and having the owner
 /// perform basic secret lifecycle operations.
@@ -27,66 +25,13 @@ async fn shared_folder_secret_lifecycle() -> Result<()> {
     let shared_folder =
         folders.iter().find(|f| f.name() == folder_name).unwrap();
 
-    let (meta, secret) = mock::note(TEST_ID, TEST_ID);
-    let SecretChange { id: secret_id, .. } = device
-        .owner
-        .create_secret(meta, secret, shared_folder.id().into())
-        .await?;
-
-    let (row, _) = device
-        .owner
-        .read_secret(&secret_id, Some(shared_folder.id()))
-        .await?;
-
-    assert!(matches!(row.secret(), Secret::Note { .. }));
-
-    let new_value = "<new value>";
-    let (_, meta, _) = row.into();
-    device
-        .owner
-        .update_secret(
-            &secret_id,
-            meta,
-            Some(Secret::Note {
-                text: new_value.into(),
-                user_data: Default::default(),
-            }),
-            shared_folder.id().into(),
-        )
-        .await?;
-
-    // Sign out and then re-authenticate to check
-    // shared folder access from sign in
-
-    device.owner.sign_out().await?;
-
-    let key: AccessKey = password.into();
-    device.owner.sign_in(&key).await?;
-
-    // Check we can read a secret created in the previous session
-    let (row, _) = device
-        .owner
-        .read_secret(&secret_id, Some(shared_folder.id()))
-        .await?;
-    let value = if let Secret::Note { text, .. } = row.secret() {
-        text.expose_secret().to_owned()
-    } else {
-        panic!("expecting a secret note");
-    };
-    assert_eq!(new_value, &value);
-
-    // Create another secret
-    let (meta, secret) = mock::note(TEST_ID, TEST_ID);
-    let SecretChange { id: secret_id, .. } = device
-        .owner
-        .create_secret(meta, secret, shared_folder.id().into())
-        .await?;
-
-    // Check secret deletion
-    device
-        .owner
-        .delete_secret(&secret_id, shared_folder.id().into())
-        .await?;
+    super::assert_shared_folder_lifecycle(
+        &mut device.owner,
+        shared_folder.id(),
+        password,
+        TEST_ID,
+    )
+    .await?;
 
     device.owner.sign_out().await?;
 
